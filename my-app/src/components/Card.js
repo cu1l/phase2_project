a94ab5ef6495daf1ad89de10e6694f3f1b6938b1
Gail Hobbs <gbhobbs97@gmail.com>
2023-03-08 14:09:22 -0800

    update toggle fave colors

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react"
 
 function Card({ destination }) {
-    const [favorite, setFavorite] = useState("false")
+    const [favorite, setFavorite] = useState(false)
 
     const togglefav = () => {
         setFavorite(!favorite)
-        console.log(favorite)
     }
 
     const {name, image, description} = destination
@@ -23,7 +22,7 @@ function Card({ destination }) {
             </div>
             <div className="extra content">
                 <span className="right floated star icon" onClick={togglefav}>
-                <i className={favorite ? "star icon" : "yellow star icon"}></i>
+                <i className={favorite ? "yellow star icon" : "star icon"}></i>
                 </span>
             </div>
         </div>
